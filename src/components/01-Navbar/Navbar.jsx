a462385bfb0d38e99a23adecf5acfbc85e3eb075
iloveolya <savchukadrian222@gmail.com>
2023-03-29 21:07:31 +0300

    Update Navbar.jsx

--- src/components/01-Navbar/Navbar.jsx
+++ src/components/01-Navbar/Navbar.jsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 const Navbar = ({ date }) => {
-  const [finishTime] = useState(date.getTime());
+  // const [finishTime] = useState(date.getTime());
   // const [[diffDays, diffH, diffM, diffS], setDiff] = useState([0, 0, 0, 0]);
-  const [tick, setTick] = useState(false);
+  // const [tick, setTick] = useState(false);
 
-  useEffect(() => {
-    const diff = (finishTime - new Date()) / 1000;
-    if (diff < 0) return; // время вышло
-    setDiff([
-      Math.floor(diff / 86400), // дни
-      Math.floor((diff / 3600) % 24),
-      Math.floor((diff / 60) % 60),
-      Math.floor(diff % 60),
-    ]);
-  }, [tick, finishTime]);
+  // useEffect(() => {
+  //   const diff = (finishTime - new Date()) / 1000;
+  //   if (diff < 0) return; // время вышло
+  //   setDiff([
+  //     Math.floor(diff / 86400), // дни
+  //     Math.floor((diff / 3600) % 24),
+  //     Math.floor((diff / 60) % 60),
+  //     Math.floor(diff % 60),
+  //   ]);
+  // }, [tick, finishTime]);
 
-  useEffect(() => {
-    const timerID = setInterval(() => setTick(!tick), 1000);
-    return () => clearInterval(timerID);
-  }, [tick]);
+  // useEffect(() => {
+  //   const timerID = setInterval(() => setTick(!tick), 1000);
+  //   return () => clearInterval(timerID);
+  // }, [tick]);
 
   return (
     <div className={styles.underNavbar}>
